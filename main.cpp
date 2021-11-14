d11f2d4ccc3811c5c68fd3fbf2311ba16a66f1d9
5209482 <91668718+5209482@users.noreply.github.com>
2021-11-14 13:14:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+   cout<<"inserisci n"
+   cin>>n;
+    int long primo=0, secondo=1, numero=primo+secondo;
+   int i=0;
+   while (numero<=n) {
+      if(i==0) cout<<1<<endl;
+      else {
+         cout<<numero<<endl;
+         primo=secondo;
+         secondo=numero;
+         numero=secondo+primo;
+      }
+   }
+   i++;
 }
+}
+    return 0;
